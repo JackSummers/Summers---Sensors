4ea99bd5f97331f1f390ccb508ba9a8efbb58275
Programming <Programming@10.90.43.194>
2015-09-28 16:12:39 -0700

    2nd commit after running on roboRIO.

--- src/org/usfirst/frc/team1165/robot/subsystems/MyAccelerometer.java
+++ src/org/usfirst/frc/team1165/robot/subsystems/MyAccelerometer.java
@@ -5,6 +5,7 @@ package org.usfirst.frc.team1165.robot.subsystems;
 
 import org.usfirst.frc.team1165.robot.commands.ReportMyAccelerometer;
 
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -13,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class MyAccelerometer extends Subsystem {
     
-	private  edu.wpi.first.wpilibj.BuiltInAccelerometer accelerometer;
+	private BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
 	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -26,21 +27,21 @@ public class MyAccelerometer extends Subsystem {
     /**
      * @return X value of accelerometer
      */
-    public double X() {
+    public double getX() {
     	return accelerometer.getX();
     }
     
     /**
      * @return Y value of accelerometer
      */
-    private double Y() {
+    private double getY() {
     	return accelerometer.getY();
     }
     
     /**
      * @return Z value of accelerometer
      */
-    private double Z() {
+    private double getZ() {
     	return accelerometer.getZ();
     }
     
@@ -48,9 +49,9 @@ public class MyAccelerometer extends Subsystem {
      * Reports status of X, Y, & Z values of accelerometer to the SmartDashboard.
      */
     public void report(){
-    	SmartDashboard.putNumber("X = ", X());
-    	SmartDashboard.putNumber("X = ", Y());
-    	SmartDashboard.putNumber("X = ", Z());
+    	SmartDashboard.putNumber("X = ", getX());
+    	SmartDashboard.putNumber("Y = ", getY());
+    	SmartDashboard.putNumber("Z = ", getZ());
     	
     }
 }
